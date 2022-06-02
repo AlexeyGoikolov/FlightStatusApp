734e9b9b510722d9e0a092327717b951b0b3439a
Alexey Goikolov <goikolov.a.d@gmail.com>
2022-06-02 15:18:09 +0600

    Добавил репозиторий для обработки запросов

---
+++ FlightStatusApp/Repositories/FlightRepository.cs
@@ -0,0 +1,46 @@
+﻿using FlightStatusApp.Context;
+using FlightStatusApp.Models;
+
+namespace FlightStatusApp.Repositories;
+
+public class FlightRepository : IRepository
+{
+    private readonly FlightContext _context;
+
+    public FlightRepository(FlightContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<Flight> GetAllStatuses()
+    {
+        return _context.Statuses;
+    }
+
+    public IQueryable<Flight> GetFilteredStatuses(string origin, string destination)
+    {
+        IQueryable<Flight> statuses = _context.Statuses;
+        if (!string.IsNullOrEmpty(origin))
+            statuses = statuses.Where(s => s.Origin == origin);
+        if (!string.IsNullOrEmpty(destination))
+            statuses = statuses.Where(s => s.Destination == destination);
+        return statuses;
+    }
+
+    public void Create(Flight status)
+    {
+        _context.Statuses.Add(status);
+        Save();
+    }
+
+    public void Update(Flight status)
+    {
+        _context.Statuses.Update(status);
+        Save();
+    }
+
+    public async void Save()
+    {
+       await _context.SaveChangesAsync();
+    }
+}
