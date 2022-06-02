484be04d7fcc5d30512b29b8e134708982b1171a
Alexey Goikolov <goikolov.a.d@gmail.com>
2022-06-02 16:29:44 +0600

    Добавил репозиторий для пользователя. Поправил репозиторий статусов полета

--- FlightStatusApp/Repositories/FlightRepository/FlightRepository.cs
+++ FlightStatusApp/Repositories/FlightRepository/FlightRepository.cs
@@ -1,5 +1,6 @@
 ﻿using FlightStatusApp.Context;
 using FlightStatusApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FlightStatusApp.Repositories;
 
@@ -12,31 +13,37 @@ public class FlightRepository : IRepository
         _context = context;
     }
 
-    public IEnumerable<Flight> GetAllStatuses()
+    public async Task<Flight> GetStatus(int id)
     {
-        return _context.Statuses;
+        return await _context.Statuses.FirstOrDefaultAsync(s => s.Id == id);
+    }
+    public async Task<IEnumerable<Flight>> GetAllStatuses()
+    {
+        return await _context.Statuses.ToListAsync();
     }
 
-    public IQueryable<Flight> GetFilteredStatuses(string origin, string destination)
+    public async Task<IEnumerable<Flight>> GetFilteredStatuses(string origin, string destination)
     {
         IQueryable<Flight> statuses = _context.Statuses;
         if (!string.IsNullOrEmpty(origin))
             statuses = statuses.Where(s => s.Origin == origin);
         if (!string.IsNullOrEmpty(destination))
             statuses = statuses.Where(s => s.Destination == destination);
-        return statuses;
+        return await statuses.ToListAsync();
     }
 
-    public void Create(Flight status)
+    public async Task<Flight> Create(Flight status)
     {
-        _context.Statuses.Add(status);
+        await _context.Statuses.AddAsync(status);
         Save();
+        return status;
     }
 
-    public void Update(Flight status)
+    public Task<Flight> Update(Flight status)
     {
         _context.Statuses.Update(status);
         Save();
+        return Task.FromResult(status);
     }
 
     public async void Save()
---
+++ FlightStatusApp/Repositories/FlightRepository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using FlightStatusApp.Models;
+
+namespace FlightStatusApp.Repositories;
+
+public interface IRepository
+{
+    public Task<Flight> GetStatus(int id);
+    public Task<IEnumerable<Flight>> GetAllStatuses();
+    public Task<IEnumerable<Flight>> GetFilteredStatuses(string origin, string destination);
+    public Task<Flight> Create(Flight status);
+    public Task<Flight> Update(Flight status);
+    public void Save();
+
+
+}
